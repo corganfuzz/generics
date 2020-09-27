464923c8b3b2e0de96e18031ff3e3934a6c50a8d
corganfuzz <corganfuzz@localhost.localdomain>
2020-09-26 23:35:07 -0500

    generic sort completed

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -1,54 +1,52 @@
 package generics;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-    static double[] selectionSort(double[] list) {
+    static <E extends Comparable<E>> void sortG(E[] list) {
+        E currentMin;
+        int currentMinIndex;
 
         for (int i = 0; i < list.length - 1; i++) {
-
-            double currentMin = list[i];
-            int currentMinIndex = i;
+            currentMin = list[i];
+            currentMinIndex = i;
 
             for (int j = i + 1; j < list.length; j++) {
-                if (currentMin > list[j]) {
+                if (currentMin.compareTo(list[j]) > 0) {
                     currentMin = list[j];
                     currentMinIndex = j;
                 }
             }
-
             if (currentMinIndex != i) {
                 list[currentMinIndex] = list[i];
                 list[i] = currentMin;
             }
         }
-        return list;
     }
 
-    static double[] ArrayInputChecker() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("How many elements do you want to enter: ");
-        int length = s.nextInt();
-        double[] myArray = new double[length];
-        System.out.print("Enter " + length + " elements, please: ");
-        for (int i = 0; i < length; i++) {
-            myArray[i] = s.nextDouble();
+    static void printList(Object[] list) {
+        for (Object o : list) {
+            System.out.print(o + " ");
         }
-        return myArray;
+        System.out.println();
     }
 
-    static void printedResult() {
-        double[] myArray = ArrayInputChecker();
-        System.out.print("\n");
-        System.out.println("Your array is " + Arrays.toString(myArray));
-        System.out.println("==============================================");
-        String printedResult = Arrays.toString(selectionSort(myArray));
-        System.out.println("Your sorted array is " + printedResult);
+    static void sortingArraysGenerically() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many elements do you want to enter: ");
+
+        int length = sc.nextInt();
+        String[] myArray = new String[length];
+
+        System.out.print("Enter " + length + " only " + " elements, please: ");
+        for (int i = 0; i < length; i++) {
+            myArray[i] = sc.next();
+        }
+        sortG(myArray);
+        printList(myArray);
     }
 
     public static void main(String[] args) {
-        printedResult();
+        sortingArraysGenerically();
     }
 }
