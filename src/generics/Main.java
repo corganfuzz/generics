67967dc4cb8345b21417a09b14a1de1eab573198
corganfuzz <corganfuzz@localhost.localdomain>
2020-09-27 15:00:16 -0500

    finished

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -5,47 +5,53 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static <E extends Comparable<E>> void sortG(E[] list) {
-        E currentMin;
-        int currentMinIndex;
 
-        for (int i = 0; i < list.length - 1; i++) {
-            currentMin = list[i];
-            currentMinIndex = i;
+    static class Sort {
+        static <E extends Comparable<E>> void sortG(E[] list) {
+            E currentMin;
+            int currentMinIndex;
 
-            for (int j = i + 1; j < list.length; j++) {
-                if (currentMin.compareTo(list[j]) > 0) {
-                    currentMin = list[j];
-                    currentMinIndex = j;
+            for (int i = 0; i < list.length - 1; i++) {
+                currentMin = list[i];
+                currentMinIndex = i;
+
+                for (int j = i + 1; j < list.length; j++) {
+                    if (currentMin.compareTo(list[j]) > 0) {
+                        currentMin = list[j];
+                        currentMinIndex = j;
+                    }
+                }
+                if (currentMinIndex != i) {
+                    list[currentMinIndex] = list[i];
+                    list[i] = currentMin;
                 }
             }
-            if (currentMinIndex != i) {
-                list[currentMinIndex] = list[i];
-                list[i] = currentMin;
+        }
+
+        static void printList(Object[] list) {
+            for (Object o : list) {
+                System.out.print(o + " ");
             }
+            System.out.println();
         }
-    }
 
-    static void printList(Object[] list) {
-        for (Object o : list) {
-            System.out.print(o + " ");
+        static void sortingArraysGenerically() {
+            System.out.println();
+            System.out.println("SORTING (length and array itself required)");
+            System.out.println("==========================================\n");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("How many elements do you want to enter: ");
+
+            int length = sc.nextInt();
+            String[] myArray = new String[length];
+
+            System.out.print("Enter " + length + " elements ONLY, please: ");
+            for (int i = 0; i < length; i++) {
+                myArray[i] = sc.next();
+            }
+            sortG(myArray);
+            printList(myArray);
         }
-        System.out.println();
-    }
-
-    static void sortingArraysGenerically() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("How many elements do you want to enter: ");
-
-        int length = sc.nextInt();
-        String[] myArray = new String[length];
-
-        System.out.print("Enter " + length + " elements ONLY, please: ");
-        for (int i = 0; i < length; i++) {
-            myArray[i] = sc.next();
-        }
-        sortG(myArray);
-        printList(myArray);
     }
 
     static class Search {
@@ -68,6 +74,9 @@ public class Main {
         }
 
         static void linearSearchListAndCollections() {
+            System.out.println();
+            System.out.println("LINEAR SEARCH");
+            System.out.println("=============\n");
             Integer[] list = {1, 4, 4, 2, 5, -3, 6, 2};
             Integer searchKey = Integer.valueOf("2");
             ArrayList<Integer> arrList = new ArrayList<>(Arrays.asList(list));
@@ -80,10 +89,35 @@ public class Main {
         }
     }
 
+    static class Menu {
+        static int displayMenu() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println();
+            System.out.print("==========================================\n");
+            System.out.println("MAKE A SELECTION");
+            System.out.println("==========================================\n");
+            System.out.println("1 - Sorting for any kind of Arrays");
+            System.out.println("2 - Linear search an specific array generically ");
+            System.out.println("3 - Exit ");
+            return sc.nextInt();
+        }
+
+        static void itemExecution(int choice) {
+            switch (choice) {
+                case 1 -> Sort.sortingArraysGenerically();
+                case 2 -> Search.linearSearchListAndCollections();
+                case 3 -> System.exit(0);
+                default -> System.out.println("Invalid Selection only 1-3 available");
+            }
+        }
+
+        static void userSelection() {
+            int userChoice = Menu.displayMenu();
+            Menu.itemExecution(userChoice);
+        }
+    }
+
     public static void main(String[] args) {
-        Search.linearSearchListAndCollections();
-//        sortingArraysGenerically();
-
-
+        Menu.userSelection();
     }
 }
